9813a720658c4a7a58fcd779155b0096dcb51c3a
ntalydevs <ntalydevs@gmail.com>
2026-02-26 11:44:09 +0200

    feat: render quick info cards component based on data coming from the outlet context

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,3 +1,25 @@
+import type { BodyType, CardType } from "@/@types/dashboard";
+import { QuickInfoCard } from "@/components/cards/dashboard/quick-info-card";
+import { QuickInfoContainer } from "@/components/containers/dashboard/quick-info-container";
+import { useOutletContext } from "react-router-dom";
+
 export function Dashboard() {
-  return <p>dashboard</p>;
+  const dashboardContext: BodyType = useOutletContext();
+
+  const cards: CardType = dashboardContext.cards;
+
+  return (
+    <QuickInfoContainer>
+      {cards.map((d) => (
+        <QuickInfoCard
+          key={d.id}
+          title={d.title}
+          bodyMessage={d.value}
+          icon={d.icon}
+          iconColor={d.iconColor}
+          footerMessage={d.footerMessage}
+        />
+      ))}
+    </QuickInfoContainer>
+  );
 }
