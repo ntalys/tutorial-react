0af7ccc03cbb515d561bbbc385b6e57aa20a56e9
ntalydevs <ntalydevs@gmail.com>
2026-03-06 01:28:45 +0200

    chore: change component name

---
+++ src/pages/calendar-page.tsx
@@ -0,0 +1,21 @@
+import { TableContainer } from "@/components/containers/table/table-container";
+import { VisitorCalendar } from "@/components/custom/calendar/visitor-calendar";
+import { useState } from "react";
+import { useOutletContext } from "react-router-dom";
+
+export function CalendarPage() {
+  const visitorCalendarContext: BodyType = useOutletContext();
+
+  const [date, setDate] = useState<Date | undefined>(new Date());
+
+  const formattedDate = date?.toLocaleDateString("en-US", {
+    month: "long",
+    year: "numeric",
+  });
+
+  return (
+    <TableContainer headerTitle={formattedDate}>
+      <VisitorCalendar />
+    </TableContainer>
+  );
+}
