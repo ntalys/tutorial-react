c2834d205cdcd8b128e7b398f19066de373b5a7a
ntalydevs <ntalydevs@gmail.com>
2026-03-05 03:18:09 +0200

    fix: style issue

--- src/components/containers/dashboard/base-container.tsx
+++ src/components/containers/dashboard/base-container.tsx
@@ -1,3 +1,3 @@
 export function BaseContainer({ children }: { children: React.ReactNode }) {
-  return <div className="flex flex-1 w-full px-3 pb-3">{children}</div>;
+  return <div className="flex flex-1 w-full px-3 pb-3 pt-3">{children}</div>;
 }
