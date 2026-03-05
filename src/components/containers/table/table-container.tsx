0bbbb070f2fb21fc5c795133f38f5bc802427554
ntalydevs <ntalydevs@gmail.com>
2026-03-05 03:19:42 +0200

    fix: added new prop

--- src/components/containers/table/table-container.tsx
+++ src/components/containers/table/table-container.tsx
@@ -5,11 +5,17 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 
-export function TableContainer({ children }: { children: React.ReactNode }) {
+export function TableContainer({
+  children,
+  headerTitle,
+}: {
+  children: React.ReactNode;
+  headerTitle?: string;
+}) {
   return (
     <Card className="flex mx-auto w-full">
       <CardHeader className="capitalize text-3xl font-semibold px-6">
-        <CardTitle>Recent Activity</CardTitle>
+        <CardTitle>{headerTitle}</CardTitle>
       </CardHeader>
 
       <CardDescription className="px-6">{children}</CardDescription>
