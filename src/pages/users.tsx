583ccf23b402aa00b1b883b29aee5f7d47217213
ntalydevs <ntalydevs@gmail.com>
2026-03-05 12:33:15 +0200

    feat: use user table on user page

--- src/pages/users.tsx
+++ src/pages/users.tsx
@@ -1,3 +1,16 @@
+import type { BodyType } from "@/@types/users";
+import { BaseContainer } from "@/components/containers/dashboard/base-container";
+import UsersTable from "@/components/table/users/users-table";
+import { useOutletContext } from "react-router-dom";
+
 export function Users() {
-  return <p>USers</p>;
+  const usersContext: BodyType = useOutletContext();
+
+  const users = usersContext.users;
+
+  return (
+    <BaseContainer>
+      <UsersTable users={users} />
+    </BaseContainer>
+  );
 }
